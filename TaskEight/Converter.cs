97172ec8c4b85402de970ffb257d1dc4794c742d
Юрий <xattab.rus@gmail.com>
2016-11-19 14:54:53 +0500

    Добавил 2 разных конвертера.

--- TaskEight/Converter.cs
+++ TaskEight/Converter.cs
@@ -1,10 +1,55 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Globalization;
+using System.Linq;
 
 namespace TaskEight
 {
     public static class Converter
     {
+        // Аргумент notUsed добавлена тут для того чтоб компилятор не ругался на схожие сигнатуры методов
+        // Я просто хочу показать разные подходы решения задачи конвертации
+        public static object Convert(string propName, string value, Type type, object notUsed) 
+        {
+            var propertyType = type.GetProperty(propName)?.PropertyType;
+
+            if(propertyType == null)
+                throw new ArgumentException("Не удается найти данное свойство");
+            
+            var converter = TypeDescriptor.GetConverter(propertyType);
+
+            if (!converter.IsValid(value))
+                throw new Exception($"Значение {value} не подходит для свойства {propName}");
+
+            return converter.ConvertFromString(null, CultureInfo.InvariantCulture, value);
+        }
+
+
+        public static object Convert(string propName, string value, Type template)
+        {
+            var property = template.GetProperty(propName);
+
+            if (property == null)
+                throw new ArgumentException("Не допустимое свойство!");
+
+            if (property.PropertyType == typeof(int))
+                return int.Parse(value);
+
+            if (property.PropertyType == typeof(double))
+                return double.Parse(value, CultureInfo.InvariantCulture);
+
+            if (property.PropertyType == typeof(string))
+                return value;
+
+            if (property.PropertyType == typeof(int?))
+            {
+                int res;
+                return int.TryParse(value, out res) ? (int?)res : null;
+            }
+
+            throw new InvalidCastException("Не удается найти тип приведения для данного свойста!");
+        }
+
         public static object Convert(string propName, string value)
         {
             switch (propName)
@@ -12,15 +57,15 @@ namespace TaskEight
                 case "Name":
                     return value;
                 case "Ozone":
-                {
-                    int res;
-                    return int.TryParse(value, out res) ? (int?)res : null;
-                }
+                    {
+                        int res;
+                        return int.TryParse(value, out res) ? (int?)res : null;
+                    }
                 case "Solar.R":
-                {
-                    int res;
-                    return int.TryParse(value, out res) ? (int?) res : null;
-                }
+                    {
+                        int res;
+                        return int.TryParse(value, out res) ? (int?)res : null;
+                    }
                 case "Wind":
                     return double.Parse(value, CultureInfo.InvariantCulture);
                 case "Temp":
